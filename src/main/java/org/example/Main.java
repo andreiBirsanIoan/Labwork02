bc9eebe8924164ffa6a7b3307bad5099ba9da986
Bîrsan Ioan-Andrei <ioanandrei.birsan@ulbsibiu.ro>
2026-05-18 12:28:40 +0300

    modificare functii

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> numbers = getNumbers();
-        numbers.stream().forEach(System.out::println);
+        //numbers.stream().forEach(System.out::println);
+        numbers.stream().filter(n->n%2==0).forEach(System.out::println);
+        numbers.stream().filter(n->n%2==1).forEach(System.out::println);
     }
 
     public static ArrayList<Integer> getNumbers() {
